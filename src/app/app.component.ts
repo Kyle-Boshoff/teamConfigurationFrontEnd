69a550b6904d366ee327dd2a7e9b18a735640cfd
Kyle Boshoff <kyleboshoffdp@gmail.com>
2023-09-11 16:00:15 +0200

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'teamConfigurationFrontEnd';
+  users:any;
+
+  displayedColumns: string[] = [ 'Name', 'Surname', 'Email','Team',];
+  teams: any;
+
+  constructor(private userService:UserService){}
+  ngOnInit(): void {
+    this.getUsers();
+    this.getTeams();
+  }
+
+  getUsers(){
+    this.userService.getUsers().subscribe(data=>{
+      this.users= data;
+      console.log(data);
+      
+    })
+  }
+
+  getTeams(){
+    this.userService.getTeams().subscribe(data=>{
+      this.teams= data;
+      console.log(data);
+      
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {NgFor} from '@angular/common';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,MatTableModule,
+    MatFormFieldModule, MatSelectModule, NgFor, MatInputModule, FormsModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
